5d2a80e528d5928662b27da47706e03926bb54a1
chelsea zhai <chelsea.zhai@gmail.com>
2013-02-22 13:08:45 +0800

    update walkwork project

--- src/com/richitec/walkwork/LoginActivity.java
+++ src/com/richitec/walkwork/LoginActivity.java
@@ -1,20 +1,21 @@
 package com.richitec.walkwork;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class LoginActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity_layout);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity_layout);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.login_activity_layout, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.login_activity_menu, menu);
+		return true;
+	}
+
 }
